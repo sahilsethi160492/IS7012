8ba7fe7994f7f8cfc154ce74e4cf68a745ba5691
sahilsethi160492 <sethisl@mail.uc.edu>
2021-09-10 19:49:43 -0400

    Initial check-in of module RecruitCatsahilsethi160492

---
+++ Pages/Profiles/Bhavesh.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RecruitCatsahilsethi160492.Pages.Profiles
+{
+    public class BhaveshModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
